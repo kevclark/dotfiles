c4fcf2a8c9812e47853b354464f1021bf63be218
Kevin Clark <kev@devlap.com>
2022-11-13 23:05:42 +0000

    Updated neogit and diffview with whichkey bindings

--- .config/nvim/lua/user/ngit.lua
+++ .config/nvim/lua/user/ngit.lua
@@ -33,17 +33,7 @@ neogit.setup {
   integrations = {
     -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `sindrets/diffview.nvim`.
     -- The diffview integration enables the diff popup, which is a wrapper around `sindrets/diffview.nvim`.
-    --
-    -- Requires you to have `sindrets/diffview.nvim` installed.
-    -- use { 
-    --   'TimUntersberger/neogit', 
-    --   requires = { 
-    --     'nvim-lua/plenary.nvim',
-    --     'sindrets/diffview.nvim' 
-    --   }
-    -- }
-    --
-    diffview = false  
+    diffview = true
   },
   -- Setting any section to `false` will make the section not render at all
   sections = {
@@ -76,7 +66,7 @@ neogit.setup {
       -- Adds a mapping with "B" as key that does the "BranchPopup" command
       ["B"] = "BranchPopup",
       -- Removes the default mapping of "s"
-      ["s"] = "",
+      ["S"] = "",
     }
   }
 }
--- .config/nvim/lua/user/whichkey.lua
+++ .config/nvim/lua/user/whichkey.lua
@@ -113,26 +113,22 @@ local mappings = {
 
   g = {
     name = "Git",
-    g = { "<cmd>GV<cr>", "Log" },
-    G = { "<cmd>GV!<cr>", "Log current file" },
+    g = { "<cmd>Neogit<cr>", "Status" },
+    l = { "<cmd>DiffviewFileHistory<cr>", "Log" },
+    L = { "<cmd>DiffviewFileHistory %<cr>", "Log current file" },
     j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
     k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-    l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+    b = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
     p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
     r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
     R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
     s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
-    u = {
-      "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
-      "Undo Stage Hunk",
-    },
+    u = { "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", "Undo Stage Hunk" },
     o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
-    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+    B = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
     c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
-    d = {
-      "<cmd>Gitsigns diffthis HEAD<cr>",
-      "Diff",
-    },
+    d = { "<cmd>DiffviewOpen<cr>", "Diff" },
+    q = { "<cmd>tabc<cr>", "Close Diffview" },
   },
 
   m = {
