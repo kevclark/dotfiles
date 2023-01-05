e79d26bc8134b43b89d28a61089ff7b1fb9cb833
Kevin Clark <kev@devlap.com>
2023-01-05 23:24:11 +0000

    start of refactor and moving to lazy.nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,15 +4,16 @@ local options = {
   backup = false,                          -- creates a backup file
   swapfile = false,                        -- dont create swap file
   -- cmdheight = 2,                           -- more space in the neovim command line for displaying messages
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  conceallevel = 0,                        -- so that `` is visible in markdown files
+  completeopt = { "menu", "menuone", "noselect" }, -- mostly just for cmp
+  conceallevel = 3,                        -- Hide * markup for bold and italic
   fileencoding = "utf-8",                  -- the encoding written to a file
   hlsearch = true,                         -- highlight all matches on previous search pattern
   incsearch = true,                        -- Incremental search
   ignorecase = true,                       -- ignore case in search patterns
 --  mouse = "a",                             -- allow the mouse to be used in neovim
+  pumblend = 10,                           -- Popup blend
   pumheight = 10,                          -- pop up menu height
-  showcmd = true,                          -- Show (partial) command in status line.
+  showcmd = true,                          -- Show (kal) command in status line.
   showmatch = true,                        -- Show matching brackets.
   showmode = false,                         -- Don't need this anymore as mode will appear in lualine
   showtabline = 2,                         -- always show tabs
@@ -21,12 +22,14 @@ local options = {
   splitbelow = true,                       -- force all horizontal splits to go below current window
   splitright = true,                       -- force all vertical splits to go to the right of current window
   termguicolors = true,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 100,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+  timeoutlen = 200,                        -- time to wait for a mapped sequence to complete (in milliseconds)
   updatetime = 300,                        -- faster completion (4000ms default)
   writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
   expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 2,                          -- the number of spaces inserted for each indentation
-  tabstop = 2,                             -- insert 4 spaces for a tab
+  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
+  softtabstop = 4,
+  tabstop = 4,                             -- insert 4 spaces for a tab
+  formatoptions = "jcroqlnt",              -- tcqj
   cursorline = true,                       -- highlight the current line
   number = true,                           -- set numbered lines
   relativenumber = true,                   -- set relative numbered lines
@@ -40,14 +43,15 @@ local options = {
   undodir = HOME .. "/.vim/undodir",       -- undo file per file being edited
   undofile = true,                         -- undodir must exist
   colorcolumn = "100",                     -- file width column indicator
+  wildmode = "longest:full,full",          -- Command-line completion mode
 }
 
 local globals = {
     -- vim global options
-    -- wildmenu = true,
-    -- wildignore = "*.pyc",                       -- ignore files
-    -- wildignore = wildignore .. ",*_build/*",
-    -- wildignore = wildignore .. ",**/.git/*",
+    mapleader = " ",
+    maplocalleader = " ",
+    wildmenu = true,
+    wildignore = "*.pyc,*_build/*,**/.git/*",                       -- ignore files
 }
 
 -- allows neovim to access the system clipboard
@@ -65,7 +69,3 @@ end
 for k, v in pairs(globals) do
   vim.g[k] = v
 end
-
-vim.cmd "set whichwrap+=<,>,[,],h,l"
-vim.cmd [[set iskeyword+=-]]
-vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
