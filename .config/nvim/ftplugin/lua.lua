76270e63152091f08989d6de1c4de12b22e4adc2
Kevin Clark <kev@devlap.com>
2023-01-18 22:41:34 +0000

    Added nvim file type specific buffer settings and added font to Alacritty

---
+++ .config/nvim/ftplugin/lua.lua
@@ -0,0 +1,6 @@
+-- Buffer settings for lua files
+vim.bo.expandtab = true -- convert tabs to spaces
+vim.bo.shiftwidth = 2 -- the number of spaces inserted for each indentation
+vim.bo.softtabstop = 2
+vim.bo.tabstop = 2 -- insert 2 spaces for a tab
+vim.wo.colorcolumn = "120" -- file width column indicator
---
+++ .config/nvim/ftplugin/python.lua
@@ -0,0 +1,6 @@
+-- Buffer settings for python files
+vim.bo.expandtab = true -- convert tabs to spaces
+vim.bo.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.bo.softtabstop = 4
+vim.bo.tabstop = 4 -- insert 4 spaces for a tab
+vim.wo.colorcolumn = "80" -- file width column indicator
