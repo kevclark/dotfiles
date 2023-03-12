71f73a6a513fd668895beecb7038cea910a5f450
Kevin Clark <kev@devlap.com>
2023-03-12 22:32:05 +0000

    Fix fg esc keymap bringing up whichkey menu

--- .config/nvim/lua/plugins/ui_override.lua
+++ .config/nvim/lua/plugins/ui_override.lua
@@ -30,7 +30,17 @@ return {
   {
     "folke/which-key.nvim",
     opts = function()
-      require("which-key").register({
+      local wk = require("which-key")
+      wk.setup({
+        triggers_blacklist = {
+          -- WhichKey already blacklists j and k in insert mode, which is why the
+          -- common mapping jk, used to quickly enter normal mode, does not bring
+          -- up the WhichKey menu. However fg is used from insert mode to enter
+          -- normal mode when using colemak rather than querty keyboard layout.
+          i = { "f" },
+        },
+      })
+      wk.register({
         ["<leader>t"] = { name = "+terminal" },
       })
     end,
