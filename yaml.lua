1a5edf13acfd57445e3d50775526fde78bfda5f5
Kevin Clark <kevin.clark@amiosec.com>
2023-05-11 12:04:41 +0100

    Added yaml formatting template for vim and alias for bat

---
+++ yaml.lua
@@ -0,0 +1,6 @@
+-- Buffer settings for lua files
+vim.bo.expandtab = true -- convert tabs to spaces
+vim.bo.shiftwidth = 4 -- the number of spaces inserted for each indentation
+vim.bo.softtabstop = 4
+vim.bo.tabstop = 4 -- insert 2 spaces for a tab
+vim.wo.colorcolumn = "81" -- file width column indicator
