fb2cff6031b4c07ef125e3ef61d620184d72d427
Kevin Clark <kev@devlap.com>
2024-08-06 23:32:30 +0100

    Empty which-key setup hides default groups from LazyVim

--- .config/nvim/lua/plugins/ui_override.lua
+++ .config/nvim/lua/plugins/ui_override.lua
@@ -1,13 +1,4 @@
 return {
-  -- which-key
-  {
-    "folke/which-key.nvim",
-    opts = function()
-      local wk = require("which-key")
-      wk.setup()
-    end,
-  },
-
   {
     "iamcco/markdown-preview.nvim",
     build = ":call mkdp#util#install()",
