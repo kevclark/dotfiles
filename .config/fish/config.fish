dc7c307d85a3c3f476738c225c5c0281c7792b57
Kevin Clark <kev@devlap.com>
2025-01-07 01:48:19 +0000

    Add HOME/bin to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 export EDITOR=nvim
 export VISUAL=nvim
 export SUDO_EDITOR=nvim
+fish_add_path $HOME/bin
+
 set fish_greeting
 if status is-interactive
     # Commands to run in interactive sessions can go here
