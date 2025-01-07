855498f3fcdb35fbac46c8a8f7ec6676d36f7a87
Kevin Clark <kev@devlap.com>
2025-01-07 21:59:53 +0000

    Ignore grc wrapper for ls
    Fisher plugins installed:
    jhillyerd/plugin-git
    oh-my-fish/plugin-grc
    patrickf1/fzf.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ export EDITOR=nvim
 export VISUAL=nvim
 export SUDO_EDITOR=nvim
 fish_add_path $HOME/bin
+# ignore grc wrapper for ls and use our own override functin for ls
+set -U grc_plugin_ignore_execs ls
 
 set fish_greeting
 if status is-interactive
