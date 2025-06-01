036b5ec88863ae56b268204870bf9e20156cfc46
Kevin Clark <kev@devlap.com>
2025-06-01 23:22:54 +0100

    Added checkmate.nvim

---
+++ .config/nvim/lua/plugins/checkmate.lua
@@ -0,0 +1,8 @@
+return {
+    "bngarren/checkmate.nvim",
+    ft = "markdown", -- Lazy loads for Markdown files matching patterns in 'files'
+    opts = {
+        -- your configuration here
+        -- or leave empty to use defaults
+    },
+}
